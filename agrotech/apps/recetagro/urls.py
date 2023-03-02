8f447306679c8ab9b2522f4483a6c976c4974979
lein33 <sleandrojoel@hotmail.com>
2023-03-02 06:44:33 -0500

    configuracion base de datos

---
+++ agrotech/apps/recetagro/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('', views.home,name='home'),
+	#path('test',views.whatsAppWebhook,name='whatsapp-webhook'),
+    
+
+]
--- agrotech/apps/recetagro/views.py
+++ agrotech/apps/recetagro/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse
 
+import json
+
+from django.http import HttpResponse
+
+from django.template.loader import get_template
+import os
 # Create your views here.
+def home(request):
+    return render(request,'index.html',{})
