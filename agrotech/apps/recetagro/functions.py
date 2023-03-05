3a2ce752883f00f0474400979eec0b777e4a118a
lein33 <sleandrojoel@hotmail.com>
2023-03-04 21:37:09 -0500

    configuracion whatsapp

---
+++ agrotech/apps/recetagro/functions.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+import requests
+import json
+from .models import *
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+import pdfkit
+from django.template.loader import get_template
+from .aigenerations import *
+import os
+from threading import Thread
+
+def handleWhatsAppChat(fromId, profileName, phoneId,text):
+    pass
+def sendWhatsAppMessage(phoneNumber, message):
+    headers = {"Authorization": settings.WHATSAPP_TOKEN}
+    payload = {
+        "messaging_product":"whatsapp",
+        "recipient_type":"individual",
+        "to":phoneNumber,
+        "type":"text",
+        "text":{
+            "body":message
+        }
+    }
+    response = requests.post(settings.WHATSAPP_URL,headers=headers,json=payload)
+    ans = response.json()
+    return ans
