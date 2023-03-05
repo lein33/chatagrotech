10429ae83d6b70f49f7c134f45d1252ee172089a
lein33 <sleandrojoel@hotmail.com>
2023-03-05 08:09:36 -0500

    personaliazndo estilos

--- agrotech/agrotech/urls.py
+++ agrotech/agrotech/urls.py
@@ -22,5 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.recetagro.urls'),name='recetagro'),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
