317d11e4df1ad92aad301e65f834dd69a56367a4
lein33 <sleandrojoel@hotmail.com>
2023-03-04 22:08:00 -0500

    configuracion whatsapp

--- agrotech/apps/recetagro/urls.py
+++ agrotech/apps/recetagro/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),
-	#path('test',views.whatsAppWebhook,name='whatsapp-webhook'),
+	path('test',views.whatsAppWebhook,name='whatsapp-webhook'),
     
 
 ]
