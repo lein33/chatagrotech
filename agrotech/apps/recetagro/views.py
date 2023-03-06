bad8931b0fc9110a9123edff0cae5b875097df16
lein33 <sleandrojoel@hotmail.com>
2023-03-05 19:38:32 -0500

    añadiendo ai

--- agrotech/apps/recetagro/views.py
+++ agrotech/apps/recetagro/views.py
@@ -13,7 +13,7 @@ import pdfkit
 from django.template.loader import get_template
 import os
 def home(request):
-    
+
     return render(request,'about.html',{'about':About.objects.all()})
 
 @csrf_exempt
