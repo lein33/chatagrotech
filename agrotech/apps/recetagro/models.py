2787162f229c2b98b057002b487a64f668c45fcc
lein33 <sleandrojoel@hotmail.com>
2023-03-05 19:47:27 -0500

    añadiendo ai

--- agrotech/apps/recetagro/models.py
+++ agrotech/apps/recetagro/models.py
@@ -36,7 +36,8 @@ class About(models.Model):
     ultima_edicion = models.DateTimeField(blank=True,null=True)
     
     def __str__(self):
-        return self.perfil.user
+        return self.comentario
+    
     def save(self,*args,**kwargs):
         if self.fecha_creacion is None:
             self.fecha_creacion = timezone.localtime(timezone.now())
