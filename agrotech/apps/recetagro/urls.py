191d2e2a738eea8bbb1cccce97e7e7a92a3dd4c2
lein33 <sleandrojoel@hotmail.com>
2023-03-06 22:49:37 -0500

    agregando la vista modelo about

--- agrotech/apps/recetagro/urls.py
+++ agrotech/apps/recetagro/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path,include
 
 from . import views
-
+app_name = 'recetagro'
 urlpatterns = [
     path('', views.home.as_view(),name='home'),
     path('servicios', views.Servicios.as_view(),name='servicios'),
