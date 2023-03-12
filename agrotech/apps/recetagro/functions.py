67bd662a19014236997a71e86aac536af166f31f
lein33 <sleandrojoel@hotmail.com>
2023-03-12 16:49:05 -0500

    chat

--- agrotech/apps/recetagro/functions.py
+++ agrotech/apps/recetagro/functions.py
@@ -50,6 +50,8 @@ def CrearServicios(chat):
     servicio_dos_ia = CustomThread(target=servicio_dos,args=(chat.tipo_industria,chat.tamaño_industria))
     servicio_tres_ia = CustomThread(target=servicio_tres,args=(chat.tipo_industria,chat.tamaño_industria))
 
+    
+
     descripcion_industria.start()
     servicios_ia.start()
     razones_ia.start()
@@ -147,77 +149,3 @@ def handleWhatsAppChat(fromId, profileName, phoneId,text):
         sendWhatsAppMessage(fromId,message)
         #sendWhatsAppMedia(fromId)
 
-def handleWhatsAppChatHome(fromId, profileName, phoneId,text):
-    try:
-        chat = ChatSessions.objects.get(perfil__phoneNumber=fromId)
-    except:
-        if User.objects.filter(username=phoneId).exists():
-            usuario = User.objects.get(username=phoneId)
-            user_profile = usuario.profile
-        
-        else:
-            usuario = User.objects.create_user(
-            username=phoneId,
-            email='te3ster@gfkfm-tech',
-            password='04.desnutryfy',
-            first_name=profileName)
-
-            user_profile = Perfil.objects.create(
-            user=usuario,
-            phoneNumber=fromId,
-            phoneId=phoneId)
-            
-        chat = ChatSessions.objects.create(perfil=user_profile)
-        message ="Bienvenido al asistente Agrotech  home 🧔‍♂️🧔👩‍🦳🌰"
-        sendWhatsAppMessage(fromId,message)
-    
-    if chat.tipo_industria:
-        if chat.tipo_servicio:
-            if chat.tamaño_industria:
-                if chat.comentario:
-                    message ="danos un momento"
-                    sendWhatsAppMessage(fromId,message)
-                    CrearServicios(chat)
-                    return ''
-                else:
-                    chat.comentario=text
-                    chat.save()
-                    message="Nostros tenemos lo que buscas"
-                    sendWhatsAppMessage(fromId,message)
-                
-            else:
-                try:
-                    type =  int(text.replace(' ',''))
-                    if type == 1:
-                        chat.tamaño_industria='pequeña'
-                        chat.save()
-                        message="Ingresa un comentario"
-                        sendWhatsAppMessage(fromId,message)
-                    elif type == 2:
-                        chat.tamaño_industria='mediana'
-                        chat.save()
-                        message="Ingresa unn comentario"
-                        sendWhatsAppMessage(fromId,message)
-                    elif type == 3:
-                        chat.tamaño_industria='grande'
-                        chat.save()
-                        message="ingresa un comentario"
-                        sendWhatsAppMessage(fromId,message)
-                    else:
-                        message="intentalo otra vez"
-                        sendWhatsAppMessage(fromId,message)      
-                except:
-                    message="intentalo otra vez"
-                    sendWhatsAppMessage(fromId,message)
-
-        else:
-            chat.tipo_servicio=text
-            chat.save()
-            message="Que tamaño necesitas"
-            sendWhatsAppMessage(fromId,message)
-    else:
-        chat.tipo_industria=text
-        chat.save()
-        message="Porfavor, Ahora ingresa tipo de servicio"
-        sendWhatsAppMessage(fromId,message)
-        #sendWhatsAppMedia(fromId)
